b954597054e37f5d136843641907c6fe0508c198
Timendus <mail@timendus.com>
2024-09-25 17:59:09 +0200

    Give the LLM a couple of tools to work with (WIP)

--- meshbot/ollama_llm.py
+++ meshbot/ollama_llm.py
@@ -1,12 +1,21 @@
 import requests
+import os
+from dotenv import dotenv_values
 
 from .meshwrapper import MeshtasticClient, Message
 
-system_prompt = """
-You are a helpful chatbot on the Meshtastic network. You talk a bit like a radio
-HAM, but not too much. The primary language of this part of the network is
-Dutch. But users can talk to you in any language. Just be kind and reply in the
-same language.
+config = {
+    **dotenv_values(".env"),
+    **dotenv_values("production.env"),
+    **dotenv_values("development.env"),
+    **os.environ,
+}
+
+system_prompt = f"""
+You are Meshbot, a helpful chatbot on the Meshtastic network. You talk a bit
+like a radio HAM. The primary language of this part of the network is
+{config["OLLAMA_LANGUAGE"]}. But users can talk to you in any language. Just be
+kind and reply in the same language.
 
 Remember that Meshtastic is unlicensed and does not use call signs. Also
 remember that Meshtastic uses the LoRa protocol, which can work reliably with
@@ -44,19 +53,66 @@ Information:
 
 """
 
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_signal_strength",
+            "description": "Get the signal strength for a node",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "node": {
+                        "type": "string",
+                        "description": "The ID or short name of the node for which to get the signal strength, e.g. !9a34ed2b or R3NL",
+                    },
+                },
+                "required": ["node"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_hops",
+            "description": "Get the number of hops for a node",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "node": {
+                        "type": "string",
+                        "description": "The ID or short name of the node for which to get the number of hops, e.g. !9a34ed2b or R3NL",
+                    },
+                },
+                "required": ["node"],
+            },
+        },
+    },
+]
+
 conversations = {}
+client = None
 
 
-def handle(
-    message: Message, meshtasticClient: MeshtasticClient, endpoint: str, model: str
-) -> bool:
+def handle(message: Message, meshtasticClient: MeshtasticClient) -> bool:
+    # Store for later use
+    client = meshtasticClient
+
     # Only reply to text messages
     if message.type != "TEXT_MESSAGE_APP":
         return False
 
-    # Only reply if an endpoint and a model have been configured
-    if not endpoint or not model:
+    # Only reply if the required Ollama settings have been configured
+    if not (
+        "OLLAMA_API" in config
+        and "OLLAMA_MODEL" in config
+        and "OLLAMA_LANGUAGE" in config
+    ):
         return False
+
+    # Check if model is available
+    # models = requests.get(config["OLLAMA_API"] + "/tags").text
+    # print(models)
 
     if message.private_message():
         identifier = message.fromNode.id
@@ -78,8 +134,13 @@ def handle(
 
     # Are we in a conversation?
     if identifier in conversations:
-        conversations[identifier].append({"role": "user", "content": message.text})
-        reply = reply_from_ollama(conversations[identifier], endpoint, model)
+        conversations[identifier].append(
+            {
+                "role": "user",
+                "content": f"Node {message.fromNode.id} says: {message.text}",
+            }
+        )
+        reply = reply_from_ollama(conversations[identifier])
         if reply != "" and reply != "NOTHING":
             message.reply("🤖 " + reply)
         return True
@@ -93,7 +154,7 @@ def handle(
                 + str(gather_relevant_stats(message, meshtasticClient)),
             }
         ]
-        reply = reply_from_ollama(conversations[identifier], endpoint, model)
+        reply = reply_from_ollama(conversations[identifier])
         message.reply("🤖🧠 Started LLM conversation")
         if reply != "" and reply != "NOTHING":
             message.reply("🤖 " + reply)
@@ -102,19 +163,44 @@ def handle(
     return False
 
 
-def reply_from_ollama(conversation: list, endpoint: str, model: str):
+def reply_from_ollama(conversation: list):
     request = {
-        "model": model,
+        "model": config["OLLAMA_MODEL"],
         "messages": conversation,
         "stream": False,
+        "tools": tools,
     }
 
-    try:
-        result = requests.post(endpoint + "/chat", json=request)
-    except ConnectionError:
-        return f"Could not reach the Ollama server at this time."
-    if not result.ok:
-        return f"Did not get a valid result from Ollama :/ Status: {result.status}"
+    working = True
+    while working:
+        try:
+            result = requests.post(config["OLLAMA_API"] + "/chat", json=request)
+        except ConnectionError:
+            return f"Could not reach the Ollama server at this time."
+        if not result.ok:
+            return f"Did not get a valid result from Ollama :/ Status: {result.status_code}"
+
+        result = result.json()
+        tool_calls = result.get("message", {}).get("tool_calls", False)
+
+        if tool_calls:
+            print("tool call!")
+            print(tool_calls)
+            for call in tool_calls:
+                function = call.get("function", {})
+                arguments = function.get("arguments", {})
+                match function.get("name", None):
+                    case "get_signal_strength":
+                        return_value = get_signal_strength(**arguments)
+                    case "get_hops":
+                        return_value = get_hops(**arguments)
+                    case _:
+                        assert False, "Invalid function name in function call from LLM"
+                conversation.append({"role": "tool", "content": return_value})
+
+            print(request)
+        else:
+            working = False
 
     return (
         result.json()
@@ -129,23 +215,32 @@ def gather_relevant_stats(message: Message, meshtasticClient: MeshtasticClient) 
     }
     if message.private_message():
         stats["user"] = {
-            "rssi": message.fromNode.rssi,
-            "snr": message.fromNode.snr,
             "shortName": message.fromNode.shortName,
             "longName": message.fromNode.longName,
             "id": message.fromNode.id,
-            "hops": message.fromNode.hopsAway,
         }
     else:
         stats["users"] = [
             {
-                "rssi": node.rssi,
-                "snr": node.snr,
                 "shortName": node.shortName,
                 "longName": node.longName,
                 "id": node.id,
-                "hops": node.hopsAway,
             }
             for node in meshtasticClient.nodelist().nodes.values()
         ]
     return stats
+
+
+def get_signal_strength(node: str) -> str:
+    node = client.nodelist().find(node)
+    return str(
+        {
+            "snr": node.snr,
+            "rssi": node.rssi,
+        }
+    )
+
+
+def get_hops(node: str) -> str:
+    node = client.nodelist().find(node)
+    return str({"hopsAway": node.hopsAway})
